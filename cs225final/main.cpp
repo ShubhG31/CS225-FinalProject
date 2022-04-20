ac573338ed845ad48d3573dd5c40a8e2cd056c70
Dev Goyal <devg2@illinois.edu>
2022-04-20 21:05:26 +0000

    testing

--- cs225final/main.cpp
+++ cs225final/main.cpp
@@ -7,14 +7,14 @@ int main(){
     Graph h("nodedataset.csv","edgedataset.csv");
 
     // Uncomment lines 8-12 to see the nodedataset being parsed
-    //h.makeNodeList("nodedataset.csv");
+    // h.makeNodeList("nodedataset.csv");
 
     // for(auto i: h.nodeList){
     //     std::cout<< i.id << " " << i.longitude << " " << i.latitude <<  std::endl;
     // }
 
     // Uncomment lines 15-20 to see the edgedataset being parsed
-    //h.makeEdgeList("edgedataset.csv");
+    // h.makeEdgeList("edgedataset.csv");
     // for(auto j: h.edgelist){
     //     for(auto i: j.second){
     //         cout<<i.first << " " << i.second <<endl;
---
+++ cs225final/tests.cpp
@@ -0,0 +1,5 @@
+#include "catch/catch.hpp"
+
+TEST_CASE("Test hello world", "") {
+  REQUIRE( 1 + 1 == 2 );
+}
