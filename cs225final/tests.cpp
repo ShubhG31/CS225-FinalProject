314a45671d3e2a131dae6e79183426286db7fc41
Dev Goyal <devg2@illinois.edu>
2022-04-24 20:54:51 +0000

    made 1 test case

--- cs225final/tests.cpp
+++ cs225final/tests.cpp
@@ -1,5 +1,13 @@
 #include "catch/catch.hpp"
+#include <string>
+#include "graph.h"
+
+using namespace std;
 
 TEST_CASE("Test hello world", "") {
-  REQUIRE( 1 + 1 == 2 );
+  string node_data = "node.csv";
+  string edge_data = "edge.csv";
+  Graph h(node_data, edge_data);
+  vector<int> sol{0,1,2,3};
+  REQUIRE(sol == h.findShortestPath(0,4));
 }
